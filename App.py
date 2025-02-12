a4d0ef994029a8e5a5714bfea666c0bdce939c04
DiegoCorrea07 <diego.correa.cumbicus07@outlook.com>
2025-02-11 19:06:40 -0500

    Update App.py

--- App.py
+++ App.py
@@ -174,6 +174,7 @@ if st.session_state.pantalla == 1:
                 font-size: 2.25rem; /* Tamaño equivalente a st.title */
                 font-weight: bold; /* Texto en negrita */
                 text-transform: uppercase; /* Convertir el texto a mayúsculas*/
+                margin-top: 20px;
                 margin-bottom: 25px; /* Separación con contenido siguiente */
             }
         </style>
